9af0be6a38b6c14a8e26c6a05206068cc515552f
HypThaNyx <wesleyyds@hotmail.com>
2022-06-27 21:17:56 -0300

    chore: include basic spring files

---
+++ src/main/java/com/hypthanyx/anacomb/AnaCombApplication.java
@@ -0,0 +1,13 @@
+package com.hypthanyx.anacomb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaCombApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaCombApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hypthanyx/anacomb/AnaCombApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypthanyx.anacomb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaCombApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
